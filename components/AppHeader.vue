ef7907783a2a43ac18fceff36931909e0d79d8a4
mertcnl <mertmehmetcinel@gmail.com>
2025-12-14 17:31:25 +0300

    feat: Enhance header layout with user actions, search bar, and navigation items

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -29,16 +29,42 @@
                 <img class="header-logo" src="../assets/images/watsons-logo.png" alt="">
             </div>
             <div class="header-main-right-container">
-                <div class="search-bar-wrapper">
-                    <Icon name="fa-solid:search" class="search-icon" size="18" />
-
-                    <input type="text" placeholder="Write the product or category you are looking for" />
+                <div class="search-bar-container">
+                    <div class="search-bar-wrapper">
+                        <Icon name="fa-solid:search" class="search-icon" size="18" />
+                        <input type="text" placeholder="Write the product or category you are looking for" />
+                    </div>
                 </div>
 
+                <div class="user-cart-container">
+                    <div class="user-actions">
+                        <div class="user-info">
+                            <span class="greeting">Hello,</span>
+                            <span class="username">Clara Clara</span>
+                        </div>
+
+                        <img src="../assets/images/user-img.jpg" alt="Clara Clara" class="user-avatar" />
+
+                        <Icon name="fa-solid:chevron-down" class="dropdown-icon" size="12" />
+                    </div>
+
+                    <div class="divider"></div>
+                    <div class="cart-wrapper">
+                        <Icon name="fa7-solid:basket-shopping" class="cart-icon" size="24" />
+                        <span class="cart-badge">2</span>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="header-nav-menu">
-            Menü Alanı
+            <a class="header-nav-item" href="#">CAMPAIGNS</a>
+            <a class="header-nav-item" href="#">PERSONAL CARE</a>
+            <a class="header-nav-item" href="#">SKIN CARE</a>
+            <a class="header-nav-item" href="#">MAKE - UP</a>
+            <a class="header-nav-item" href="#">MOTHER & BABY</a>
+            <a class="header-nav-item" href="#">MEN CARE</a>
+            <a class="header-nav-item" href="#">HEALTHY LIFE</a>
+            <a class="header-nav-item" href="#">BRANDS</a>
         </div>
     </header>
 </template>
